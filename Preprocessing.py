fcce90a9e668c3dbd5da80e35986f1e8ff1b86b2
tal <alfital2>
2020-07-25 19:23:37 +0300

    best version of Preprocessing.py

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,25 +4,17 @@ import numpy as np
 
 class Preprocessing():
 
-    def __init__(self, train, test, structure, remove_nans ,numBins=None):
-        self.train_df = train
-        self.test_df = test
-        self.structure_dict_file = self.list_of_strings_to_dict(structure)
-        self.discretization(self.train_df, remove_nans, numBins)
-        self.discretization(self.test_df, remove_nans, numBins)
+    def __init__(self,**kwargs):
+        self.bins_dict_range = {}
+        self.train_df = kwargs['train']
+        self.test_df = kwargs['test']
+        self.structure_dict_file = self.list_of_strings_to_dict(kwargs['structure'])
+        self.discretization(self.train_df,kwargs['missing_values'] , kwargs['strategy'], kwargs['number_of_bins'])
+        self.discretization_test(self.test_df, kwargs['missing_values'] , kwargs['strategy'])
 
     def init_data_frames(self, csv_file_name):
         return pd.read_csv(csv_file_name).applymap(lambda s: s.lower() if type(s) == str else s)
 
-    def make_bins(self, data, NumBins, column):
-        bins = {}
-        binsIntervals = np.append(
-            np.array([-float('inf')]), np.linspace(min(data), max(data), NumBins - 1))
-        binsIntervals = np.append(binsIntervals, np.array([float('inf')]))
-
-        for i in range(len(binsIntervals)-1):
-            bins['bin{0}'.format(i+1)] = (binsIntervals[i], binsIntervals[i+1])
-        return bins
 
     def list_of_strings_to_dict(self, Structure):
         structure_dict = {}
@@ -32,41 +24,89 @@ class Preprocessing():
                 x for x in last[1:-1].split(',')]
         return structure_dict
 
-    def discretization(self, data, remove_nans, NumBins=None):
+    def add_inf_values(self, bins_array):
+        bins_array[0] = np.array([-float('inf')])
+        bins_array[len(bins_array) - 1] = np.array([float('inf')])
 
-        self.__replaceNans(data)
+    def discretization(self, data, Nones, strategy, number_of_bins=None):
+        self.Nones_action(data,Nones)
+        if strategy == 'equal_width':
+            self.equal_width_or_frequency(data, pd.cut, number_of_bins)
 
+        elif strategy == 'equal_frequency':
+            self.equal_width_or_frequency(data, pd.qcut, number_of_bins)
+
+    def Nones_action(self,data,Nones):
+        if Nones == "remove":
+            self.__dropNans(data)
+        elif Nones == "replace":
+            self.__replaceNans(data)
+
+    def equal_width_or_frequency(self, data, func, number_of_bins):
         for column in (list(data)[:-1]):
             if self.structure_dict_file[column] == 'NUMERIC':
-                self.__replaceCol(self.create_new_column_acording_bins(
-                    NumBins, column, data), column, data)
+                binned_column = func(np.array(data[column]), number_of_bins, duplicates='drop', retbins=True)
+                bins = [x + 1 for x in range(len(binned_column[1]) - 1)]
+                binned_column = func(np.array(data[column]), number_of_bins, labels=bins, duplicates='drop',
+                                     retbins=True)
+                # getting the array of bins the way pandas made it
+                self.bins_dict_range[column] = binned_column[1]
+                self.__replaceCol(binned_column[0], column, data)
+                self.add_inf_values(self.bins_dict_range[column])
 
-    def create_new_column_acording_bins(self, NumBins, column, data):
-        bins = self.make_bins(data[column], NumBins, column)
-        column_acording_bins = []
+    def discretization_test(self, data, Nones, strategy):
+        self.Nones_action(data,Nones)
+        if strategy == 'equal_width':
+            self.create_new_column_according_bins(data)
+        elif strategy == 'equal_frequency':
+            self.create_new_column_according_bins(data)
 
-        for elm in data[column]:
-            for Bin in bins.keys():
-                if elm >= (bins[Bin])[0] and elm <= (bins[Bin])[1]:
-                    column_acording_bins.append(Bin)
-                    break
-        return column_acording_bins
+    def create_new_column_according_bins(self, data):
+        for column in (list(data)[:-1]):
+            binned_column = []
+            if self.structure_dict_file[column] == 'NUMERIC':
+                bins = self.bins_dict_range[column]
+                for elm in data[column]:
+                    for Bin in range(len(bins)):
+                        if bins[Bin] <= elm <= bins[Bin + 1]:
+                            binned_column.append(Bin + 1)
+                            break
+                data[column] = binned_column
 
     def __replaceNans(self, data):
         data.fillna(data.mode().iloc[0], inplace=True)
 
-    def __dropNans(self,data):
+    def __dropNans(self, data):
         data.dropna(inplace=True)
 
     def __replaceCol(self, newCol, column, data):
         data[column] = newCol
+
 
 def Preprocessing_adapter(**kwargs):
     train = kwargs['train']
     test = kwargs['test']
     structure = kwargs['structure']
     numBins = kwargs['number_of_bins']
-    remove_nans = True if kwargs['missing_values'] == 'remove_nans' else False
-    inst = Preprocessing( train, test, structure, remove_nans ,numBins)
-    return  {'test':inst.test_df ,'train':inst.train_df}
+    Nones = kwargs['missing_values']
+    inst = Preprocessing(train, test, structure, Nones, numBins)
+    return {'test': inst.test_df, 'train': inst.train_df}
 
+
+###########
+tmp = open("Structure.txt", "r")
+structure_file = []
+for line in tmp:
+    structure_file.append(line)
+tmp.close()
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
+########################
+
+kwargs={'train' : train, 'test':test, 'structure' :structure_file,  'number_of_bins': 3,
+        'missing_values' : 'replace', 'strategy':'equal_frequency' }
+#Preprocess = Preprocessing(train, test, structure_file, True, 'equal_frequency', 3)
+# Preprocess = Preprocessing(train, test, structure_file, "replace", 'equal_width', 3)
+
+Preprocess = Preprocessing(**kwargs)
+print(Preprocess.train_df['duration'][19])
